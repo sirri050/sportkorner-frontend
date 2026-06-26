3290016bbab92c6563cd25f5e2b4bee1e44ebc10
irfan-alyy <irfanserboon562@gmail.com>
2026-06-27 01:43:25 +0500

    fix: with catch route file

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ['@swc/helpers'],
   
   images: {
     dangerouslyAllowLocalIP: true,
